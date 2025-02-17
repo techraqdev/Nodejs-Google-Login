7126ae33e5aab086d8926cc669dc01df2f82d9b9
gvijgiri <ganesh.vijigiri@techraq.com>
2025-02-17 13:22:35 +0530

    Added email validation

--- GLogin/Backend/src/Auth/authMiddleware.ts
+++ GLogin/Backend/src/Auth/authMiddleware.ts
@@ -2,6 +2,9 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import dotenv from "dotenv";
 dotenv.config();
+import { z } from "zod";
+
+const emailSchema = z.string().email();
 
 passport.use(
   new GoogleStrategy(
@@ -11,6 +14,21 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
+      // const email = profile.emails?.[0]?.value;
+      // if (!email) {
+      //   return done(new Error("Email is required!"), undefined);
+      // }
+
+      // // Validate Email Format
+      // const emailCheck = emailSchema.safeParse(email);
+      // if (!emailCheck.success) {
+      //   return done(new Error("Invalid email format!"), undefined);
+      // }
+
+      // // Optional: Check if email is from a specific domain
+      // if (!email.endsWith("@techraq.com")) {
+      //   return done(new Error("Only company emails allowed!"), undefined);
+      // }
       done(null, profile);
     }
   )
